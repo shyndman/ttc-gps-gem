5084697ba71079142d786db89cb9a7df9d975a93
shyndman <scotty.hyndman@gmail.com>
2010-10-22 19:12:28 -0400

    ttc-gps.rb no longer runs any code. additional gem dependencies

--- lib/ttc-gps.rb
+++ lib/ttc-gps.rb
@@ -4,13 +4,3 @@ require 'ttc-gps/models'
 require 'ttc-gps/service'
 require 'rubygems'
 require 'geokit'
-require 'yaml'
-
-include TTC
-
-service = TTCService.new
-route = service.get_route(ROUTES[0])
-
-me = Geokit::LatLng.new 43.6038156, -79.4930397
-puts route.contains? me
-puts (route.get_closest_stops me, "westbound").to_yaml
